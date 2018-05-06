3c4a49fe88fd8aeed71fc191ece945af6013e28e
sanjeevss3 <sanjeevss.chaudhary@gmail.com>
2018-05-06 15:36:05 +0530

    Code push to git hub test

---
+++ recruitment/src/main/java/autoit_script/Test_GitHub_push.java
@@ -0,0 +1,18 @@
+package autoit_script;
+
+
+/**
+*
+*@author Sanjeev Chaudhary
+*https://github.com/sanjeevss3/
+*
+*/
+
+public class Test_GitHub_push {
+
+	public static void main(String[] args) {
+		System.out.println("GitHub code push Test");
+
+	}
+
+}
