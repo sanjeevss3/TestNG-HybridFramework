ba3b022282aa7ef66d005d0b381d79344dec0307
sanjeevss3 <sanjeevss.chaudhary@gmail.com>
2018-05-18 18:42:16 +0530

    Updated on May-18-2018

---
+++ recruitment/src/main/java/com/retestfailedtestcases/TestCase1.java
@@ -0,0 +1,19 @@
+package com.retestfailedtestcases;
+
+import org.testng.annotations.Test;
+
+/**
+*
+*@author Sanjeev Chaudhary
+*https://github.com/sanjeevss3/
+*
+*/
+
+public class TestCase1 {
+	
+	@Test
+	public void testAJS() {
+		System.out.println("Aviation Job is working as expected");
+	}
+
+}
---
+++ recruitment/src/main/java/com/retestfailedtestcases/TestCase3.java
@@ -0,0 +1,21 @@
+package com.retestfailedtestcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+*
+*@author Sanjeev Chaudhary
+*https://github.com/sanjeevss3/
+*
+*/
+
+public class TestCase3 {
+	
+	@Test
+	public void testTJS() {
+		Assert.assertTrue(false);
+		System.out.println("Travel Job Search is working as expected");
+	}
+
+}
