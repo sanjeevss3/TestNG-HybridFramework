d1cc38f81d739858c085b6f1aaa955cb79986b24
sanjeevss3 <sanjeevss.chaudhary@gmail.com>
2018-05-23 07:29:18 +0530

    Updated on May-23-2018

--- recruitment/src/main/java/com/recruitment/open_all_sites/Beta_AJS_Open_Links.java
+++ recruitment/src/main/java/com/recruitment/open_all_sites/Beta_AJS_Open_Links.java
@@ -30,42 +30,49 @@ public class Beta_AJS_Open_Links {
 
 		// WebDriver driver=new ChromeDriver();
 
-		// String baseUrl = "https://www.aviationjobsearch.com/";
+		 String baseUrl = "https://www.aviationjobsearch.com/";
 		// WebDriver driver = new ChromeDriver();
-		// driver.manage().window().maximize();
-		// driver.get("https://www.aviationjobsearch.com/");
+		 driver.manage().window().maximize();
+		 driver.get("https://www.aviationjobsearch.com/");
 
-		// String underConsTitle = "Under Construction: Mercury Tours";
+		String underConsTitle = "Under Construction: Mercury Tours";
 
-		// driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
-		// driver.get(baseUrl);
+		driver.get(baseUrl);
 
-		/*
-		 * Thread.sleep(3000); System.out.println("Pushing code to GitHub test.");
-		 * //driver.findElement(By.xpath(".//*[@id='top_line']/div/div[2]/div/span")).
-		 * click();
-		 * 
-		 * driver.findElement(By.xpath(".//*[@id='cookie_policy']/div/div[3]/span")).
-		 * click();
-		 * 
-		 * List<WebElement> linkElements = driver.findElements(By.tagName("a"));
-		 * String[] linkTexts = new String[linkElements.size()]; int i = 0;
-		 * 
-		 * // extract the link texts of each link element for (WebElement e :
-		 * linkElements) { linkTexts[i] = e.getText(); i++; }
-		 * 
-		 * // test each link for (String t : linkTexts) {
-		 * driver.findElement(By.linkText(t)).click(); // if
-		 * (driver.getTitle().equals(underConsTitle)) { // System.out.println("\"" + t +
-		 * "\"" + " is under construction."); // } else { // System.out.println("\"" + t
-		 * + "\"" + " is working."); // }
-		 * 
-		 * // driver.navigate().back(); ((JavascriptExecutor)
-		 * driver).executeScript("window.open()"); ArrayList<String> tabs1 = new
-		 * ArrayList<String>(driver.getWindowHandles());
-		 * driver.switchTo().window(tabs1.get(0)); Thread.sleep(3000); } driver.quit();
-		 */
+		
+		  Thread.sleep(3000); System.out.println("Pushing code to GitHub test.");
+		  driver.findElement(By.xpath(".//*[@id='top_line']/div/div[2]/div/span")).
+		  click();
+		  
+		  driver.findElement(By.xpath(".//*[@id='cookie_policy']/div/div[3]/span")).
+		  click();
+		  
+		  List<WebElement> linkElements = driver.findElements(By.tagName("a"));
+		  String[] linkTexts = new String[linkElements.size()]; 
+		  int i = 0;
+		  
+		  // extract the link texts of each link element 
+		  for (WebElement e :
+		  linkElements) { 
+			  linkTexts[i] = e.getText(); i++; 
+			  }
+		  
+		  // test each link 
+		  for (String t : linkTexts) {
+		//  driver.findElement(By.linkText(t)).click(); 
+	//	  if(driver.getTitle().equals(underConsTitle)) { 
+//			  System.out.println("\"" + t +
+//		  "\"" + " is under construction."); 
+//			  } else { // System.out.println("\"" + t
+//		  + "\"" + " is working."); // }
+//		  
+		  // driver.navigate().back(); ((JavascriptExecutor)
+		//  driver).executeScript("window.open()"); ArrayList<String> tabs1 = new
+		 // ArrayList<String>(driver.getWindowHandles());
+	//	  driver.switchTo().window(tabs1.get(0)); Thread.sleep(3000); } driver.quit();
+		 
 	}
 
 }
