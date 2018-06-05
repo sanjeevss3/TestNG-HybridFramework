4fa5df1ddf39a7c09caaa6761c4bcf784ddf2a02
sanjeevss3 <sanjeevss.chaudhary@gmail.com>
2018-06-05 17:18:52 +0530

    Updated on June-04-2018

--- recruitment/src/main/java/com/recruitment/open_all_sites/Launch_All_Live_Sites.java
+++ recruitment/src/main/java/com/recruitment/open_all_sites/Launch_All_Live_Sites.java
@@ -43,13 +43,13 @@ public class Launch_All_Live_Sites {
 		driver.switchTo().window(tabs1.get(1));
 		
 		driver.get("http://www.simplysalesjobs.co.uk/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+		//driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs2.get(2)); 
 		driver.get("http://www.simplylawjobs.com/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+	//	driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs3 = new ArrayList<String>(driver.getWindowHandles());
@@ -61,37 +61,37 @@ public class Launch_All_Live_Sites {
 		ArrayList<String> tabs4 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs4.get(4));
 		driver.get("http://www.aviationjobsearch.com/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+		//driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs5 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs5.get(5));
 		driver.get("http://us.aviationjobsearch.com/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+		//driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs6 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs6.get(6));
 		driver.get("http://www.traveljobsearch.com/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+	//	driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs7 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs7.get(7));
 		driver.get("http://jobs.cabincrew.com/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+		//driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs8 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs8.get(8));
 		driver.get("http://jobs.natives.co.uk/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+		//driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs9 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs9.get(9));
 		driver.get("http://www.simplyengineeringjobs.com/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+		//driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs10 = new ArrayList<String>(driver.getWindowHandles());
@@ -104,55 +104,55 @@ public class Launch_All_Live_Sites {
 		ArrayList<String> tabs11 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs11.get(11));
 		driver.get("https://www.simplyhrjobs.co.uk/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+	//	driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs12 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs12.get(12));
 		driver.get("http://www.simplyofficejobs.co.uk/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+		//driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs13 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs13.get(13));
 		driver.get("http://www.simplyhoteljobs.com/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+		//driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs14 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs14.get(14));
 		driver.get("http://www.renewableenergyjobs.com/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+		//driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs15 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs15.get(15));
 		driver.get("http://us.inautomotive.com/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+		//driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs16 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs16.get(16));
 		driver.get("http://us.simplyhoteljobs.com/");
-		driver.findElement(By.linkText("Newest Jobs")).click();
+	//	driver.findElement(By.linkText("Newest Jobs")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs17 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs17.get(17));
 		driver.get("http://www.simplyitsalesjobs.co.uk/");
-		driver.findElement(By.linkText(".//*[@id='btn_search']")).click();
+	//	driver.findElement(By.linkText(".//*[@id='btn_search']")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs18 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs18.get(18));
 		driver.get("http://www.simplymediasalesjobs.co.uk/");
-		driver.findElement(By.linkText(".//*[@id='btn_search']")).click();
+	//	driver.findElement(By.linkText(".//*[@id='btn_search']")).click();
 
 		((JavascriptExecutor) driver).executeScript("window.open()");
 		ArrayList<String> tabs19 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs19.get(19));
 		driver.get("http://www.jobsinbrighton.co.uk/");
-		driver.findElement(By.linkText(".//*[@id='btn_search']")).click();
+		//driver.findElement(By.linkText(".//*[@id='btn_search']")).click();
 		
 
 		System.out.println("All LIVE sites are opened in Browser");
